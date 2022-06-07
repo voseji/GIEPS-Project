aae8b2e9ae7e04fd1ebf6e13684428a99e6ee390
Victor <vctroseji@gmail.com>
2022-06-07 12:08:27 +0100

    make it better

--- gogetit-ieps-frontend/src/pages/tables/createIncome.js
+++ gogetit-ieps-frontend/src/pages/tables/createIncome.js
@@ -1,6 +1,6 @@
-// import React from "react";
-// import React, { useState, useEffect } from 'react'
-import React, { useState } from "react";
+import { useCallback } from "react";
+// import React, { useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 // import MUIDataTable from "mui-datatables";
 import API from '../../api';
@@ -15,25 +15,23 @@ import {
   // Tabs,
   // Tab,
   TextField,
+  MenuItem,
+  Select
   // Fade,
 } from "@material-ui/core";
 
 // import { useNavigate } from "react-router-dom";
-import Select from 'react-select';
+// import Select from 'react-select';
+// import { SelectFetch } from 'react-select-fetch';
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
 
 export default function CreateIncome() {
-  
-  const [incomeId, setIncomeId] = useState(null)
-  const [incomeCategory, setIncomeCategory] = useState(null)
-  const [description, setDescription] = useState(null)
-  const [amount, setAmount] = useState(null)
-  // const navigate = useNavigate();
+
+  const [incomeId, setIncomeId] = useState("")
+  const [description, setDescription] = useState("")
+  const [amount, setAmount] = useState("")
+  const [incomeCategories, setIncomeCategories] = useState([])
+  const [incomeCategory, setIncomeCategory] = useState("");
 
   const createOneIncome = async (e) => {
     e.preventDefault();
@@ -47,83 +45,96 @@ export default function CreateIncome() {
 
 
 
-  const res1 = await API.post(`/income`, formData)
-console.log(res1)
-  // navigate("/")
+    const res1 = await API.post(`/income`, formData)
 
- 
+  }
 
-}
+  const fetchIncome = useCallback(async () => {
+    await API.get(`/income_category`).then(({ data }) => {
+      setIncomeCategories(data)
+    })
+  }, []);
+  useEffect(() => {
+    fetchIncome()
+  }, [fetchIncome])
 
   return (
     <>
       <PageTitle title="Register New Income" />
       <Grid container spacing={4}>
         <Grid item xs={12}>
-        <TextField
-                id="incomeId"
-                margin="normal"
-                placeholder="Income ID"
-                type="text"
-                fullWidth
-                value={incomeId} 
-                onChange={(event)=>{
-                  setIncomeId(event.target.value)
-                }}/>
+          <TextField
+            id="incomeId"
+            placeholder="Income ID"
+            type="text"
+            variant="outlined"
+            style={{margin: "10px 0"}}
+            fullWidth
+            value={incomeId}
+            onChange={(event) => {
+              setIncomeId(event.target.value)
+            }} />
 
-        <TextField
-                id="incomeCategory"
-                margin="normal"
-                placeholder="Income Category"
-                type="text"
-                fullWidth
-                value={incomeCategory} 
-                onChange={(event)=>{
-                  setIncomeCategory(event.target.value)
-                }}/>
 
-              
-        <TextField
-                id="description"
-                margin="normal"
-                placeholder="Description"
-                type="text"
-                fullWidth
-                value={description} 
-                onChange={(event)=>{
-                  setDescription(event.target.value)
-                }}/>
 
-        <TextField
-                id="amount"
-                margin="normal"
-                placeholder="Amount"
-                type="text"
-                fullWidth
-                value={amount} 
-                onChange={(event)=>{
-                  setAmount(event.target.value)
-                }}/> 
+
+          <TextField
+            id="description"
+            placeholder="Description"
+            type="text"
+            variant="outlined"
+            style={{margin: "10px 0"}}
+            fullWidth
+            value={description}
+            onChange={(event) => {
+              setDescription(event.target.value)
+            }} />
+
+          <TextField
+            id="amount"
+            placeholder="Amount"
+            type="text"
+            variant="outlined"
+            style={{margin: "10px 0"}}
+            fullWidth
+            value={amount}
+            onChange={(event) => {
+              setAmount(event.target.value)
+            }} />
 
           <Select
-           id="incomeCategory"
-           margin="normal" 
-          options={options} 
-          
-          />
-          <br/>  
+            id="incomeCategory"
+            variant="outlined"
+            style={{margin: "10px 0"}}
+            fullWidth
+            value={incomeCategory}
+            onChange={({target: {value: category}}) => {
+              setIncomeCategory(category);
+              console.log(category)
+            }}
 
-                  <Button
-                   variant="contained"
-                    color="primary"
-                    size="large"
-                    onClick={createOneIncome}
-                  >
-                    Submit
-                  </Button>
+          >
+            <MenuItem value={""}><em>Select Category</em></MenuItem>
+            {incomeCategories.map(category => {
+              return (<MenuItem value={category.incomeCategoryType} key={category.id}>
+                {category.incomeCategoryType}
+              </MenuItem>)
+            })}
+          </Select>
+
+          <br />
+
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={createOneIncome}
+          >
+            Submit
+          </Button>
 
         </Grid>
-       
+
       </Grid>
     </>
   );
