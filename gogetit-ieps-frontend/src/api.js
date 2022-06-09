fad19eff0347076f96cb33e4747d4419404eff3a
Victor <vctroseji@gmail.com>
2022-06-09 16:15:13 +0100

    make it better

--- gogetit-ieps-frontend/src/api.js
+++ gogetit-ieps-frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const BACKEND_HOST = 'http://127.0.0.1/index.php'
+export const BACKEND_HOST = 'http://159.223.135.118/index.php'
 
 export default axios.create({
   baseURL: `${BACKEND_HOST}/api`
